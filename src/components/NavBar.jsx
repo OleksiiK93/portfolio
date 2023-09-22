4d5792e8234d61bb5e9588e88eb109788c56b252
Oleksii Koliukh <oleksii.koliukh@gmail.com>
2023-09-22 14:05:57 +0100

    Added nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return ( 
+        <ul>
+            <li>Oleksii</li>
+            <li>oleksii.koliukh@gmail.com</li>
+            <li>Projects</li>
+        </ul>
+     );
+}
+ 
+export default NavBar;
--- src/containers/PortfolioContainer.jsx
+++ src/containers/PortfolioContainer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
+import NavBar from '../components/NavBar';
+import Projects from '../components/Projects';
+import ErrorPage from '../components/ErrorPage';
 
 const PortfolioContainer = () => {
     return ( 
         <Router>
+            <NavBar/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                </Routes>
+                <Route path="/portfolio" element={<Home/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="*" element={<ErrorPage/>}/>
+            </Routes>
         </Router>
      );
 }
